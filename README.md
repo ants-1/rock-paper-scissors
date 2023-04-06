bfe1dbffca3701adeb6faf57716ee006bf333ef5
ants-1 <anthonysalisbury98@outlook.com>
2023-04-06 18:20:56 +0100

    Edit README.md, index.html, script.js and Add style.css

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # rock-paper-scissors
+
+## Introduction
+
+This is a 'Rock Paper Scissors' game for the Odin Project. The assignments can be viewed here:
+* [Link to assignment (part 1)](https://www.theodinproject.com/lessons/foundations-rock-paper-scissors)
+* [Link to assignment (part 2)](https://www.theodinproject.com/lessons/foundations-revisiting-rock-paper-scissors)
+
+## Technologies 
+* HTML
+* CSS
+* JavaScript
--- script.js
+++ script.js
@@ -1,38 +1,109 @@
+const rockBtn = document.getElementById("rock");
+const paperBtn = document.getElementById("paper");
+const scissorBtn = document.getElementById("scissor");
+const roundResultDiv = document.getElementById("round-result");
+const gameResultDiv = document.getElementById("game-result");
+const playerScoreDiv = document.getElementById("player-score");
+const computerScoreDiv = document.getElementById("computer-score");
+const roundNumSpan = document.getElementById("round-num");
+let playAgainBtn = document.querySelector(".hidden");
+let scoreboard = document.querySelector(".scoreboard");
+let playerChoice;
+let playerScore = 0;
+let computerScore = 0;
+let roundNum = 1;
+
+rockBtn.addEventListener("click", () => {
+    playerChoice = getPlayerChoice(0);
+    game();
+});
+
+paperBtn.addEventListener("click", () => {
+    playerChoice = getPlayerChoice(1);
+    game();
+});
+
+scissorBtn.addEventListener("click", () => {
+    playerChoice = getPlayerChoice(2);
+    game();
+});
+
+playAgainBtn.addEventListener("click", () => {
+    reset();
+})
+
 function getComputerChoice() {
     let randomChoice = Math.round(Math.random() * 2);
     switch (randomChoice) {
         case 0:
-            return "ROCK";
+            return "🪨";
         case 1:
-            return "PAPER";
+            return "📜";
         case 2:
-            return "SCISSOR";
+            return "✂️";
+    }
+}
 
+function getPlayerChoice(choice) {
+    switch (choice) {
+        case 0:
+            return "🪨";
+        case 1:
+            return "📜";
+        case 2:
+            return "✂️";
     }
 }
 
 function playRound(playerSelection, computerSelection) {
-    if ((playerSelection == "ROCK") && (computerSelection == "ROCK")
-        || (playerSelection == "SCISSOR") && (computerSelection == "SCISSOR")
-        || (playerSelection == "PAPER") && (computerSelection == "PAPER")) {
-        return "Draw";
-    } else if ((playerSelection == "ROCK") && (computerSelection == "SCISSOR")
-        || (playerSelection == "PAPER") && (computerSelection == "ROCK")
-        || (playerSelection == "SCISSOR") && (computerSelection == "PAPER")) {
-        return `You Win! ${playerSelection} beats ${computerSelection}`;
-    } else if ((playerSelection == "SCISSOR") && (computerSelection == "ROCK")
-        || (playerSelection == "ROCK") && (computerSelection == "PAPER")
-        || (playerSelection == "PAPER" && computerSelection == "SCISSOR")) {
-        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    if ((playerSelection == "🪨") && (computerSelection == "🪨")
+        || (playerSelection == "✂️") && (computerSelection == "✂️")
+        || (playerSelection == "📜") && (computerSelection == "📜")) {
+        roundResultDiv.textContent = "Draw";
+        scoreboard.style.backgroundColor = "#393E46";
+    } else if ((playerSelection == "🪨") && (computerSelection == "✂️")
+        || (playerSelection == "📜") && (computerSelection == "🪨")
+        || (playerSelection == "✂️") && (computerSelection == "📜")) {
+        playerScore += 1;
+        playerScoreDiv.textContent = playerScore;
+        roundResultDiv.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+        scoreboard.style.backgroundColor = "#00ADB5";
+    } else if ((playerSelection == "✂️") && (computerSelection == "🪨")
+        || (playerSelection == "🪨") && (computerSelection == "📜")
+        || (playerSelection == "📜" && computerSelection == "✂️")) {
+        computerScore += 1;
+        computerScoreDiv.textContent = computerScore;
+        roundResultDiv.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
+        scoreboard.style.backgroundColor = "#E96479";
+    }
+    roundNum += 1
+    roundNumSpan.textContent = roundNum;
+}
+
+function game() { 
+    if (playerScore == 4) {
+        gameResultDiv.textContent = "Player Wins!";
+        playerScoreDiv.textContent = 5;
+        playAgainBtn.style.display = "block";
+    } else if (computerScore == 4) {
+        gameResultDiv.textContent = "Computer Wins!";
+        computerScoreDiv.textContent = 5;
+        playAgainBtn.style.display = "block";
+    } else {
+        playRound(playerChoice, getComputerChoice());
     }
 }
 
-function game() {
-    for (i = 0; i < 5; i++) {
-        const playerChoice = prompt("Rock, Paper or Scissors").toUpperCase();
-        const computerChoice = getComputerChoice();
-        console.log(playRound(playerChoice, computerChoice));
-    }
+function reset() {
+    playerScore = 0;
+    computerScore = 0;
+    roundNum = 1;
+    playerScoreDiv.textContent = playerScore;
+    computerScoreDiv.textContent = computerScore;
+    roundNumSpan.textContent = roundNum;
+    gameResultDiv.textContent = "Pick";
+    roundResultDiv.textContent = "First to score 5 points wins!";
+    scoreboard.style.backgroundColor = "#393E46";
+    playAgainBtn.style.display = "none";
 }
 
-game();
